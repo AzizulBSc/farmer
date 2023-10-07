2ca51bc84829c1cf20fcdc74be92a43a0c672632
Azizul Hoque <azizulh8774@gmail.com>
2023-10-07 22:57:22 +0600

    category api added

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,7 +20,7 @@ use App\Http\Controllers\UserController;
 // });
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
+Route::group(['namespace' => 'App\Http\Controllers\Api', 'middleware' => ['auth:api']], function () {
+    Route::resource('category', 'CategoryController');
 
-Route::middleware('auth:api')->group(function () {
-    Route::get('user', [UserController::class, 'user_details']);
 });
