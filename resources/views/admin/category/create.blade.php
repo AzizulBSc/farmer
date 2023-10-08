60f4eb263ac2c090228f9f478d3cc75dcfe92718
mt-azizul <a5@marsanetwork.com>
2023-10-08 13:31:36 +0600

    adminlte added

---
+++ resources/views/admin/category/create.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.main')
+@section('content')
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Create Category</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">Create Category</li>
+                </ol>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
+<section class="content">
+    <div class="row">
+        <div class="col-md-12">
+            @include('flash-message')
+
+            <div class="card">
+                <div class="card-header bg-primary">
+                    <h3 class="card-title">Create Category</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                    <form method="POST" action="{{ route('Categorys.store') }}">
+                        @csrf
+                        <div class="form-group">
+                            <label for="CategoryName">Category Name:</label>
+                            <input type="text" value="{{ old('name') }}" class="form-control" id="name" name="name" placeholder="Enter Category name" required>
+                            @error('name')
+                            <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="CategoryParticipants">Parent Category</label>
+                            <select class="form-control select2" id="parent_id" name="parent_id[]">
+                                @foreach($categories as $category)
+                                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                @endforeach
+                            </select>
+                            @error('parent_id')
+                            <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+                <!-- /.card-body -->
+            </div>
+        </div>
+    </div>
+
+</section>
+@endsection
---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.main')
+@section('styles')
+<style>
+    .event-btn {
+        /* font-size: 10px; */
+        font-weight: bold;
+        /* width: 80px;
+        height: 40px; */
+        text-align: center;
+         color: white;
+    }
+    .result-btn {
+        font-size: 11px;
+        font-weight: bold;
+        /* width: 80px;
+        height: 40px; */
+        text-align: center;
+        color: white;
+    }
+</style>
+
+@endsection
+@section('content')
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>All Category</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">All Category</li>
+                </ol>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
+<section class="content">
+    <div class="row">
+        <div class="col-md-12">
+            @include('flash-message')
+
+            <div class="card">
+                <div class="card-header bg-primary">
+                    <h3 class="card-title">All Category</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+
+                    <table class="table table-bordered table-striped text-center">
+                        <thead>
+                            <tr>
+                                <th scope="col">SL</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Action</th>
+                                <th scope="col">Result</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Add more rows as needed -->
+                        </tbody>
+                    </table>
+                    <ul class="pagination pagination-month justify-content-center">
+                        <li class="page-item">
+                            <p class="text-center">Showing {{ $Category->firstItem() }} to {{
+                                $Category->lastItem()
+                                }} of {{ $Category->total() }} entries</p>
+                            {{$Category->links('pagination::bootstrap-4')}}
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.card-body -->
+            </div>
+        </div>
+    </div>
+
+</section>
+@endSection
+@section('js')
+<script>
+</script>
+@endsection
